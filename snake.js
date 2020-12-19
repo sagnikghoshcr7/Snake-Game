c267eafc03c8ad40baddffd62a2e63cfef4dcd15
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2020-12-19 17:41:10 +0530

    Create Snake

--- snake.js
+++ snake.js
@@ -1,11 +1,34 @@
 function init() {
-    var canvas = document.getElementById('mycanvas');
+    canvas = document.getElementById('mycanvas');
     W = H = canvas.width = canvas.height = 1000;
     pen = canvas.getContext('2d');
+    cs = 50;
+
+    snake = {
+        init_len: 5,
+        color: "blue",
+        cells: [],
+        direction: "right",
+
+        createSnake: function () {
+            for (var i = this.init_len; i >= 0; i--) {
+                this.cells.push({
+                    x: i,
+                    y: 0
+                });
+            }
+        },
+
+        drawSnake: function () {
+            pen.fillReact(this.cells[i].x, this.cells[i].y, cs, cs);
+        }
+    };
+
+    snake.createSnake();
 }
 
 function draw() {
-
+    snake.drawSnake();
 }
 
 function update() {
