9c47e7656e4b17c9b3c14720052b2de43a49716e
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2020-12-19 17:24:23 +0530

    Creating Board

---
+++ snake.js
@@ -0,0 +1,21 @@
+function init() {
+    var canvas = document.getElementById('mycanvas');
+    W = H = canvas.width = canvas.height = 1000;
+    pen = canvas.getContext('2d');
+}
+
+function draw() {
+
+}
+
+function update() {
+
+}
+
+function gameloop() {
+
+}
+
+init();
+
+var f = setInterval(gameloop, 100);
